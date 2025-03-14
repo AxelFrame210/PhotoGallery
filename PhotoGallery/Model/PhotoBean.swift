495cc8d1b5f389e8e71f27d073b1ba56f929557c
AxelFrame210 <duylekhanh.jeff1002@gmail.com>
2025-03-14 10:17:37 +0700

    Fix MVVM structure and improve readability:
    refactor(view):
    - replace model instance with viewmodel instance for separation
    - move PhotoView outside of PhotoGalleryView
    - refactor code style
    
    refactor(viewmodel):
    - Add loading image from bundle
    
    refactor(model):
    - move Photo struct out of PhotoModel
    - add photos array init
    - remove Gallery()
    - refactor code style

---
+++ PhotoGallery/Model/PhotoBean.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoGallery.swift
+//  PhotoGallery
+//
+//  Created by Admin on 12/3/25.
+//
+
+import Foundation
+
+public struct GalleryBean {
+    let url: URL
+    static let dummList: [GalleryBean] = []
+}
---
+++ PhotoGallery/ViewModel/PhotoGalleryViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  PhotoGalleryViewModel.swift
+//  PhotoGallery
+//
+//  Created by Admin on 12/3/25.
+//
+
+import Foundation
